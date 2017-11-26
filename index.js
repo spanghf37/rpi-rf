2d5f9514ab975ac9719092adb2ea956968f70d78
spanghf37 <spanghf37@gmail.com>
2017-11-26 11:04:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var rpi433    = require('rpi-433'),
-    rfSniffer = rpi433.sniffer({
-      pin: 2,                     //Snif on GPIO 2 (or Physical PIN 13)
-      debounceDelay: 500          //Wait 500ms before reading another code
-    }),
+    //rfSniffer = rpi433.sniffer({
+    //  pin: 2,                     //Snif on GPIO 2 (or Physical PIN 13)
+    //  debounceDelay: 500          //Wait 500ms before reading another code
+    //}),
     rfEmitter = rpi433.emitter({
       pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
       pulseLength: 350            //Send the code with a 350 pulse length
     });
 
 // Receive (data is like {code: xxx, pulseLength: xxx})
-rfSniffer.on('data', function (data) {
-  console.log('Code received: '+data.code+' pulse length : '+data.pulseLength);
-});
+//rfSniffer.on('data', function (data) {
+//  console.log('Code received: '+data.code+' pulse length : '+data.pulseLength);
+//});
 
 // Send
 rfEmitter.sendCode(1234, function(error, stdout) {   //Send 1234
